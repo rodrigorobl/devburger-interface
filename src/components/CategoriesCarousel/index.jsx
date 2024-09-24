b1c27316a32e72d33c8b8d0a38d75a327b43345c
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2024-09-23 22:15:13 -0300

    23092024

--- src/components/CategoriesCarousel/index.jsx
+++ src/components/CategoriesCarousel/index.jsx
@@ -1,55 +1,71 @@
-import { useState, useEffect } from "react";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
+import { useState, useEffect } from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
-import { api } from "../../services/api";
-import { Container, ContainerItems, Title } from "./styles";
+import { api } from '../../services/api';
+import { CategoryButton, Container, ContainerItems, Title } from './styles';
+import { useNavigate } from 'react-router-dom';
 
 export function CategoriesCarousel() {
-	const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const navigate = useNavigate();
 
-	useEffect(() => {
-		async function loadCategories() {
-			const { data } = await api.get("/categories");
+  useEffect(() => {
+    async function loadCategories() {
+      const { data } = await api.get('/categories');
 
-			setCategories(data);
-			console.log(data);
-		}
+      setCategories(data);
+      console.log(data);
+    }
 
-		loadCategories();
-	}, []);
+    loadCategories();
+  }, []);
 
-	const responsive = {
-		superLargeDesktop: {
-			// the naming can be any, depends on you.
-			breakpoint: { max: 4000, min: 3000 },
-			items: 5,
-		},
-		desktop: {
-			breakpoint: { max: 3000, min: 1024 },
-			items: 3,
-		},
-		tablet: {
-			breakpoint: { max: 1024, min: 464 },
-			items: 2,
-		},
-		mobile: {
-			breakpoint: { max: 464, min: 0 },
-			items: 1,
-		},
-	};
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
 
-	return (
-		<Container>
-			<Title>Categorias</Title>
+  return (
+    <Container>
+      <Title>Categorias</Title>
 
-			<Carousel responsive={responsive} infinite={true} partialVisbile={false} itemClass="carousel-items">
-				{categories.map((category) => (
-					<ContainerItems key={category.id} imageUrl={category.url}>
-						<p>{category.name}</p>
-					</ContainerItems>
-				))}
-			</Carousel>
-		</Container>
-	);
+      <Carousel
+        responsive={responsive}
+        infinite={true}
+        partialVisbile={false}
+        itemClass="carousel-items"
+      >
+        {categories.map((category) => (
+          <ContainerItems key={category.id} imageUrl={category.url}>
+            <CategoryButton
+              onClick={() => {
+                navigate({
+                  pathname: '/cardapio',
+                  search: `?categoria=${category.id}`,
+                });
+              }}
+            >
+              {category.name}
+            </CategoryButton>
+          </ContainerItems>
+        ))}
+      </Carousel>
+    </Container>
+  );
 }
--- src/components/CategoriesCarousel/styles.js
+++ src/components/CategoriesCarousel/styles.js
@@ -1,62 +1,69 @@
-import styled from "styled-components";
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 export const Container = styled.div`
-    .carousel-items {
-        padding-right: 40px;
-    }
+  .carousel-items {
+    padding-right: 40px;
+  }
 
-    .react-multiple-carousel__arrow--left {
-        left: 15px;
-        top: 10px;
-    }
+  .react-multiple-carousel__arrow--left {
+    left: 15px;
+    top: 10px;
+  }
 
-    .react-multiple-carousel__arrow--right {
-        top: 10px;
-    }
+  .react-multiple-carousel__arrow--right {
+    top: 10px;
+  }
 
-    padding-left: 40px;
+  padding-left: 40px;
 `;
 
 export const Title = styled.h2`
-    font-size: 32px;
-    font-weight: 800;
-    color: #9758a6;
-    padding-bottom: 12px;
-    position: relative;
-    text-align: center;
-    margin-bottom: 40px;
-    margin-top: 20px;
+  font-size: 32px;
+  font-weight: 800;
+  color: #9758a6;
+  padding-bottom: 12px;
+  position: relative;
+  text-align: center;
+  margin-bottom: 40px;
+  margin-top: 20px;
 
-    &::after {
-        content: '';
-        position: absolute;
-        bottom: 0;
-        width: 56px;
-        height: 4px;
-        background-color: #9758a6;
-        left: calc(50% - 28px);
-    }
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    width: 56px;
+    height: 4px;
+    background-color: #9758a6;
+    left: calc(50% - 28px);
+  }
 `;
 
 export const ContainerItems = styled.div`
-    background: url('${(props) => props.imageUrl}');
-    background-position: center;
-    background-size: cover;
-    border-radius: 20px;
+  background: url('${(props) => props.imageUrl}');
+  background-position: center;
+  background-size: cover;
+  border-radius: 20px;
 
-    display: flex;
-    align-items: center;
-    padding: 20px 10px;
-    width: 100%;
-    height: 250px;
+  display: flex;
+  align-items: center;
+  padding: 20px 10px;
+  width: 100%;
+  height: 250px;
+`;
 
-    p {
-        color: #ffffff;
-        background-color: rgba(0,0,0, 0.5);
-        padding: 10px 30px;
-        border-radius: 30px;
-        font-size: 22.5px;
-        font-weight: bold;
-        margin-top: 50px;
-    }
+export const CategoryButton = styled(Link)`
+  color: #ffffff;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 30px;
+  border-radius: 30px;
+  font-size: 22.5px;
+  font-weight: bold;
+  margin-top: 50px;
+  font-weight: 500;
+  text-decoration: none;
+
+  &:hover {
+    background-color: #9758a6;
+  }
 `;
