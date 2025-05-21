f7b06f65dd860921e360c9899cf5f20a8f161047
Your Name <you@example.com>
2025-05-21 16:44:49 -0300

    desfeitovercel210525

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://devburger-api-jyne.vercel.app/:3001/',
+  baseURL: 'http://localhost:3001/',
 });
 
 api.interceptors.request.use((config) => {
